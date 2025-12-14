650c8eb6c27fc3029e7f89cdaa53d9257aa14197
Wanderson Costa <wandersonbatista_costa@hotmail.com>
2025-12-14 12:31:43 -0300

    att

--- devlab/meuapp/urls.py
+++ devlab/meuapp/urls.py
@@ -79,13 +79,17 @@ urlpatterns = [
     #path('conta/', views.conta, name='conta'),
     path('perfil/', views.perfil, name='perfil'),
     # Password change (allow users to change password from profile)
-    path('password-change/', auth_views.PasswordChangeView.as_view(
-        template_name='registration/password_change_form.html',
-        success_url='/password-change/done/'
-    ), name='password_change'),
-    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(
-        template_name='registration/password_change_done.html'
-    ), name='password_change_done'),
+    path('password-reset/', 
+         auth_views.PasswordResetView.as_view(
+             template_name='registration/password_reset_basic.html'
+         ), 
+         name='password_reset'),
+    
+    path('password-reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='registration/password_reset_done.html'
+         ), 
+         name='password_reset_done'),
 
     #path(aceitar ou rejeitar solicitação de cadastro)
     path('solicitacoes-cadastro/<int:pk>/aprovar/', views.solicitacao_cadastro_aprovar, name='solicitacao_cadastro_aprovar'),

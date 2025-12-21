2d1d56183b68b4722b3ac96c66842ee9afb8c6fb
desenvolvimentopython2025 <desenvolvimentopython7@gmail.com>
2025-12-21 09:57:57 -0300

    Update settings for production environment
    Changed DEBUG setting to False and restricted ALLOWED_HOSTS.

--- devlab/devlab/settings.py
+++ devlab/devlab/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&!8(9!z@q*b^2c$d%f&g)h@j+k-l=m#n$p^q*r(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['Caldax04.pythonanywhere.com']
 
 
 # Application definition
